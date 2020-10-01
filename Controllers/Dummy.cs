a4f4995d0406923c987d33209cb8b08f866d1f63
Neville Brown <neville.brown@gmail.com>
2020-10-01 08:55:30 +0100

    Added Dummy controller and changed solution to include test project

---
+++ Controllers/Dummy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo.Controllers
+{
+    public class Dummy
+    {
+
+        public static int DoubleMe(int input) => input * 2;
+    }
+}
